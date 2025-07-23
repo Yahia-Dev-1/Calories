7b8db9d4272dd6141fb6cdf1097f1d7cd4869e40
iiyahia <yahiapro400@gmail.com>
2025-07-23 21:34:50 +0300

    تعديل طريقة عرض الصور لاستخدام public/img في جميع المكونات

--- src/data/foods.js
+++ src/data/foods.js
@@ -1,32 +1,8 @@
-import riceImg from '/img/rice.jpg';
-import meatImg from '/img/meat.jpg';
-import chickenImg from '/img/Chicken.jpg';
-import LiverImg from '/img/Liver.jpg';
-import breadImg from '/img/Bread.jpg';
-import BaguetteImg from '/img/Baguette.jpg';
-import BoiledEggImg from '/img/BoiledEgg.jpg';
-import FriedEggImg from '/img/FriedEgg.jpg';
-import TomatoImg from '/img/Tomato.jpg';
-import CucumberImg from '/img/Cucumber.jpg';
-import LettuceImg from '/img/Lettuce.jpg';
-import PastaImg from '/img/Pasta.jpg';
-import TurkeyCheeseImg from '/img/TurkeyCheese.jpg';
-import CheddarCheeseImg from '/img/CheddarCheese.jpg';
-import MozzarellaCheeseImg from '/img/MozzarellaCheese.jpg';
-import OilImg from '/img/Oil.jpg';
-import GheeImg from '/img/Ghee.jpg';
-import BeansImg from '/img/Beans.jpg';
-import TahiniImg from '/img/Tahini.jpg';
-import ChocolateImg from '/img/Chocolate.jpg';
-import DarkChocolateImg from '/img/DarkChocolate.jpg';
-import MincedMeatBeefImg from '/img/MincedMeatBeef.jpg';
-
-
 const foods = [
   {
     id: 1,
     name: "Baladi Bread ",
-    image: breadImg,
+    image: "Bread.jpg",
     caloriesPer100g: 270, // 162 kcal / 60g * 100
     protein: 6.7, // 4g / 60g * 100
     fat: 2.5, // 1.5g / 60g * 100
@@ -35,7 +11,7 @@ const foods = [
   {
     id: 2,
     name: "French Baguette ",
-    image: BaguetteImg,
+    image: "Baguette.jpg",
     caloriesPer100g: 266, // 133 kcal / 50g * 100
     protein: 8, // 4g / 50g * 100
     fat: 1.6, // 0.8g / 50g * 100
@@ -44,7 +20,7 @@ const foods = [
   {
     id: 3,
     name: "Boiled Egg ",
-    image: BoiledEggImg,
+    image: "BoiledEgg.jpg",
     caloriesPer100g: 155, // 77.5 kcal / 50g * 100
     protein: 12.6, // 6.3g / 50g * 100
     fat: 10.6, // 5.3g / 50g * 100
@@ -53,7 +29,7 @@ const foods = [
   {
     id: 4,
     name: "Fried Egg ",
-    image: FriedEggImg,
+    image: "FriedEgg.jpg",
     caloriesPer100g: 190, // 95 kcal / 50g * 100
     protein: 12, // 6g / 50g * 100
     fat: 15, // 7.5g / 50g * 100
@@ -62,7 +38,7 @@ const foods = [
   {
     id: 5,
     name: "Tomato ",
-    image: TomatoImg,
+    image: "Tomato.jpg",
     caloriesPer100g: 18,
     protein: 0.9,
     fat: 0.2,
@@ -71,7 +47,7 @@ const foods = [
   {
     id: 6,
     name: "Cucumber ",
-    image: CucumberImg,
+    image: "Cucumber.jpg",
     caloriesPer100g: 16,
     protein: 0.7,
     fat: 0.1,
@@ -80,7 +56,7 @@ const foods = [
   {
     id: 7,
     name: "Lettuce",
-    image: LettuceImg,
+    image: "Lettuce.jpg",
     caloriesPer100g: 14,
     protein: 0.9,
     fat: 0.1,
@@ -89,7 +65,7 @@ const foods = [
   {
     id: 8,
     name: "Pasta, Dry ",
-    image: PastaImg,
+    image: "Pasta.jpg",
     caloriesPer100g: 350,
     protein: 12,
     fat: 1.5,
@@ -98,128 +74,128 @@ const foods = [
   {
     id: 9,
     name: "Cheddar Cheese ",
-    image: CheddarCheeseImg,
-    caloriesPer100g: 383, // 115 kcal / 30g * 100
-    protein: 23.3, // 7g / 30g * 100
-    fat: 31.7, // 9.5g / 30g * 100
-    carbs: 1.3, // 0.4g / 30g * 100
+    image: "CheddarCheese.jpg",
+    caloriesPer100g: 403,
+    protein: 25,
+    fat: 33,
+    carbs: 1.3,
   },
   {
     id: 10,
-    name: "Minced Meat, Beef ",
-    image: MincedMeatBeefImg,
-    caloriesPer100g: 250,
-    protein: 26,
-    fat: 16,
-    carbs: 0,
+    name: "Mozzarella Cheese ",
+    image: "MozzarellaCheese.jpg",
+    caloriesPer100g: 280,
+    protein: 18,
+    fat: 17,
+    carbs: 3,
   },
   {
     id: 11,
-    name: "Mozzarella Cheese",
-    image: MozzarellaCheeseImg,
-    caloriesPer100g: 283, // 85 kcal / 30g * 100
-    protein: 20, // 6g / 30g * 100
-    fat: 20, // 6g / 30g * 100
-    carbs: 3.3, // 1g / 30g * 100
+    name: "Turkey Cheese ",
+    image: "TurkeyCheese.jpg",
+    caloriesPer100g: 286,
+    protein: 18,
+    fat: 23,
+    carbs: 2,
   },
   {
     id: 12,
     name: "Oil ",
-    image: OilImg,
-    caloriesPer100g: 800, // 120 kcal / 15g * 100
+    image: "Oil.jpg",
+    caloriesPer100g: 884,
     protein: 0,
-    fat: 93.3, // 14g / 15g * 100
+    fat: 100,
     carbs: 0,
   },
   {
     id: 13,
     name: "Ghee ",
-    image: GheeImg,
-    caloriesPer100g: 900, // 126 kcal / 14g * 100
+    image: "Ghee.jpg",
+    caloriesPer100g: 900,
     protein: 0,
     fat: 100,
     carbs: 0,
   },
   {
     id: 14,
-    name: "Fava Beans ",
-    image: BeansImg,
-    caloriesPer100g: 110,
-    protein: 7.6,
-    fat: 0.4,
-    carbs: 19,
+    name: "Beans ",
+    image: "Beans.jpg",
+    caloriesPer100g: 76,
+    protein: 4.5,
+    fat: 0.5,
+    carbs: 13.4,
   },
   {
     id: 15,
     name: "Tahini ",
-    image: TahiniImg,
-    caloriesPer100g: 593, // 89 kcal / 15g * 100
-    protein: 17.3, // 2.6g / 15g * 100
-    fat: 53.3, // 8g / 15g * 100
-    carbs: 21.3, // 3.2g / 15g * 100
+    image: "Tahini.jpg",
+    caloriesPer100g: 595,
+    protein: 17,
+    fat: 53,
+    carbs: 21,
   },
   {
     id: 16,
     name: "Chocolate ",
-    image: ChocolateImg,
-    caloriesPer100g: 533, // 160 kcal / 30g * 100
-    protein: 6.7, // 2g / 30g * 100
-    fat: 33.3, // 10g / 30g * 100
-    carbs: 60, // 18g / 30g * 100
+    image: "Chocolate.jpg",
+    caloriesPer100g: 546,
+    protein: 4.9,
+    fat: 31,
+    carbs: 61,
   },
   {
     id: 17,
-    name: "Dark Chocolate",
-    image: DarkChocolateImg,
-    caloriesPer100g: 567, // 170 kcal / 30g * 100
-    protein: 6.7, // 2g / 30g * 100
-    fat: 40, // 12g / 30g * 100
-    carbs: 43.3, // 13g / 30g * 100
+    name: "Dark Chocolate ",
+    image: "DarkChocolate.jpg",
+    caloriesPer100g: 546,
+    protein: 4.9,
+    fat: 31,
+    carbs: 61,
   },
   {
     id: 18,
-    name: "Rice",
-    image: riceImg,
-    caloriesPer100g: 130,
-    protein: 2.7,
-    fat: 0.3,
-    carbs: 28,
-  },
-  {
-    id: 19,
-    name: "Meat",
-    image: meatImg,
+    name: "Minced Meat (Beef) ",
+    image: "MincedMeatBeef.jpg",
     caloriesPer100g: 250,
     protein: 26,
     fat: 15,
     carbs: 0,
   },
   {
+    id: 19,
+    name: "Rice ",
+    image: "rice.jpg",
+    caloriesPer100g: 130,
+    protein: 2.7,
+    fat: 0.3,
+    carbs: 28,
+  },
+  {
     id: 20,
-    name: "Chicken",
-    image: chickenImg,
-    caloriesPer100g: 165,
-    protein: 31,
-    fat: 3.6,
+    name: "Meat ",
+    image: "meat.jpg",
+    caloriesPer100g: 250,
+    protein: 26,
+    fat: 15,
     carbs: 0,
   },
   {
     id: 21,
-    name: "Liver",
-    image: LiverImg,
-    caloriesPer100g: 135,
-    protein: 20,
-    fat: 5,
-    carbs: 4,
+    name: "Chicken ",
+    image: "Chicken.jpg",
+    caloriesPer100g: 239,
+    protein: 27,
+    fat: 14,
+    carbs: 0,
   },
   {
     id: 22,
-    name: "Turkey Cheese",
-    image: TurkeyCheeseImg,
-    caloriesPer100g: 280,
-    protein: 18,
-    fat: 22,
-    carbs: 2,
+    name: "Liver ",
+    image: "Liver.jpg",
+    caloriesPer100g: 135,
+    protein: 20.4,
+    fat: 3.6,
+    carbs: 3.9,
   },
 ];
 
